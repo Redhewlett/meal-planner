968cd0eb4949d53eb15da8be7f3b1f87f975b532
Teddy <edmondteddy@hotmail.fr>
2024-01-20 17:49:11 +0100

    no using bootstrap for theming

--- src/app/components/calendar/calendar.component.ts
+++ src/app/components/calendar/calendar.component.ts
@@ -3,7 +3,6 @@ import { CommonModule } from '@angular/common';
 import { FullCalendarModule } from '@fullcalendar/angular';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import { CalendarOptions } from '@fullcalendar/core';
-import 'bootstrap/dist/css/bootstrap.css';
 @Component({
   selector: 'app-calendar',
   standalone: true,
@@ -15,6 +14,5 @@ export class CalendarComponent {
  calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
     plugins: [dayGridPlugin],
-    themeSystem: 'bootstrap5',
   };
 }
