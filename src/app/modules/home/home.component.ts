c896ded8d1f60f84f44e95c311289d3cf21a8d23
Teddy <edmondteddy@hotmail.fr>
2024-01-28 13:37:25 +0100

    html frame for the section component

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { SectionComponent } from 'src/app/components/ui/section/section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,SectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
