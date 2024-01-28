11a05ff92b1cb6baaf089824fb9ccc0c308e9134
Teddy <edmondteddy@hotmail.fr>
2024-01-28 12:55:56 +0100

    button customization and variables

--- src/app/components/ui/button/button.component.ts
+++ src/app/components/ui/button/button.component.ts
@@ -1,13 +1,16 @@
+import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
+
+export type ButtonType = 'primary' | 'secondary' | 'tertiary';
 
 @Component({
   selector: 'app-button',
   standalone: true,
-  imports: [],
+  imports: [NgClass],
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
 export class ButtonComponent {
   @Input({required: true}) text!: string;
-
+  @Input() type: ButtonType = 'primary'
 }
