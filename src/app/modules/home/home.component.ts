3fd87328db038c943d78a0754796e0ffc00c94f7
Teddy <edmondteddy@hotmail.fr>
2024-02-09 17:41:34 +0100

    implementing new data and paramater in home component sections

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { SectionComponent } from 'src/app/components/ui/section/section.component';
-
+import { sections } from './section-data/sections';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,SectionComponent],
+  imports: [HeaderComponent, SectionComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  public sections = sections;
 }
