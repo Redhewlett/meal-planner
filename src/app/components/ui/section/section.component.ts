8ff888813419531cd266b821be7a9ccd4da290d8
Teddy <edmondteddy@hotmail.fr>
2024-02-10 13:12:12 +0100

    adding animation to sections with intersection observer as directive

--- src/app/components/ui/section/section.component.ts
+++ src/app/components/ui/section/section.component.ts
@@ -1,15 +1,17 @@
 import { Component, Input } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
+import { ObserveElementDirective } from 'src/app/directives/observe-element.directive';
 @Component({
   selector: 'app-section',
   standalone: true,
-  imports: [],
+  imports: [ObserveElementDirective, CommonModule],
   templateUrl: './section.component.html',
-  styleUrl: './section.component.scss'
+  styleUrl: './section.component.scss',
 })
 export class SectionComponent {
-  @Input({required : true}) sectionTitle!: string;
+  @Input({ required: true }) sectionTitle!: string;
   @Input() sectionSubtitle: string | undefined;
-  @Input({required:true}) sectionImage!: string;
+  @Input({ required: true }) sectionImage!: string;
   @Input() sectionDirection?: 'left' | 'right';
+  @Input() index!: number;
 }
